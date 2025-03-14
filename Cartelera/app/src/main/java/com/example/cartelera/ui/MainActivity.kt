62193fdd0b51c4bbb6c9afb48b03b50e73e81c1e
Raquel1403 <raquel0239@gmail.com>
2025-03-14 13:26:05 +0100

    Empezando Cartelera

--- Cartelera/app/src/main/java/com/example/cartelera/ui/MainActivity.kt
+++ Cartelera/app/src/main/java/com/example/cartelera/ui/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.example.cartelera
+package com.example.cartelera.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.cartelera.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
